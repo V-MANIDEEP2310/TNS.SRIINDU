87dc3d4e3e84ee1b5b957a4f72077794cd26d705
vaishnavi <vaishnavi@MANIDEEP>
2025-12-23 17:36:14 +0530

    Conditional.java

---
+++ SriIndu/src/com/tnsif/intro/Conditional.java
@@ -0,0 +1,18 @@
+package com.tnsif.intro;
+
+public class Conditional {
+	public static void main(String [] args)
+	{
+		int a=10;
+		int b=20;
+		if(a>b)
+		{
+			System.out.println("if");
+		}
+		else if (a<b)
+		{
+			System.out.println("else");
+		}
+	}
+
+}

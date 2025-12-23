65ac55462b79a8bd2c0364e33d3802d2750bba35
vaishnavi <vaishnavi@MANIDEEP>
2025-12-23 14:38:10 +0530

    Relational.java

---
+++ SriIndu/src/com/tnsif/intro/Relational.java
@@ -0,0 +1,21 @@
+package com.tnsif.intro;
+
+public class Relational {
+	public static void main(String [] args)
+	{
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		c=a-b;
+		System.out.println(c);
+		c=a*b;
+		System.out.println(c);
+		c=a/b;
+		System.out.println(c);
+		c=a%b;
+		System.out.println(c);
+	}
+
+}

d29cec19ae0d05dc88900a5dd21f854767cfb1d5
vaishnavi <vaishnavi@MANIDEEP>
2025-12-23 17:14:32 +0530

    demo2.java

---
+++ SriIndu/src/com/tnsif/intro/demo2.java
@@ -0,0 +1,12 @@
+package com.tnsif.intro;
+
+public class demo2 {
+    public static void main(String[] args) {
+        
+        int m = 20;
+        do {
+            System.out.println(m);
+            m++;
+        } while (m <= 30);
+    }
+}

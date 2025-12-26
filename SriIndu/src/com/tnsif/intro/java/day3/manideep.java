a65939c5761921123781e537e9b01b1f23581f9a
vaishnavi <vaishnavi@MANIDEEP>
2025-12-26 19:15:36 +0530

    manideep.java manideepdemo.java

---
+++ SriIndu/src/com/tnsif/intro/java/day3/manideep.java
@@ -0,0 +1,16 @@
+package com.tnsif.intro.java.day3;
+
+public class manideep {
+	String mname="Ramu";
+	int mid=59;
+	
+	manideep()
+	{
+		System.out.println("MANIDEEP NAME:"+mname);
+	}
+	manideep(int a)
+	{
+		System.out.println("MANIDEEP ID:"+mid+ "The value of a:"+a);
+	}
+
+}
---
+++ SriIndu/src/com/tnsif/intro/java/day3/manideepdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.intro.java.day3;
+
+public class manideepdemo {
+
+	public static void main(String[] args) {
+		manideep m = new manideep();
+		manideep m1 = new manideep(59);
+		// TODO Auto-generated method stub
+
+	}
+
+}

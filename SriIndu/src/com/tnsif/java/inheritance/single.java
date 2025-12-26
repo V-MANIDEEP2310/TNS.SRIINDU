3e94e163f61269af0a6981f86b6c2dd74b33e69d
vaishnavi <vaishnavi@MANIDEEP>
2025-12-26 19:50:40 +0530

    single.java singledemo.java

---
+++ SriIndu/src/com/tnsif/java/inheritance/single.java
@@ -0,0 +1,14 @@
+package com.tnsif.java.inheritance;
+
+public class single {
+	public void show()
+	{
+		System.out.println("A class");
+	}
+}
+
+class Singleinh extends single{
+	public void display() {
+		System.out.println("B class ");
+	}
+}
---
+++ SriIndu/src/com/tnsif/java/inheritance/singledemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.inheritance;
+
+public class singledemo {
+
+	public static void main(String[] args) {
+		Singleinh ob = new Singleinh();
+		ob.show();
+		ob.display();
+	
+	}
+}

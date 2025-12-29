28f2530d9a2e0a40a1a39122393da058a2cf061c
manideep <vaishnavi@MANIDEEP>
2025-12-29 23:41:16 +0530

    thiss.java thissdemo.java

---
+++ SriIndu/src/com/tnsif/java/day4/thiss.java
@@ -0,0 +1,12 @@
+package com.tnsif.java.day4;
+
+public class thiss {
+	int a=20;
+	public void show()
+	{
+		int a=10;
+		System.out.println("loacl variable"+a);
+		System.out.println("instance variable"+this.a);
+		}
+
+}
---
+++ SriIndu/src/com/tnsif/java/day4/thissdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.java.day4;
+
+public class thissdemo {
+
+	public static void main(String[] args) {
+		thiss t = new thiss();
+		t.show();
+		// TODO Auto-generated method stub
+
+	}
+
+}

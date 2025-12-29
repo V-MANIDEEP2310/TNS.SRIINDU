0b5539c719eb5a0562af6050ad41538c2e65bde4
manideep <vaishnavi@MANIDEEP>
2025-12-29 23:44:28 +0530

    polymerphismmol.java polymerphismdemo.java

---
+++ SriIndu/src/com/tnsif/java/day4/polymerphismdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.java.day4;
+
+public class polymerphismdemo {
+
+	public static void main(String[] args) {
+		polymerphismmol p = new polymerphismmol();
+		p.show();
+		p.show(56);
+		p.show("MANIDEEP",9.5);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SriIndu/src/com/tnsif/java/day4/polymerphismmol.java
@@ -0,0 +1,20 @@
+package com.tnsif.java.day4;
+
+public class polymerphismmol {
+	int pid=45;
+	String pname="BUNNY";
+	String pmail="hii@gmail.com";
+	public void show()
+	{
+		System.out.println("A:"+pid);
+	}
+	public void show(int a)
+	{
+		System.out.println("B:"+pname);
+	}
+	public void show(String a,double b)
+	{
+		System.out.println("C:"+pmail);
+	}
+
+}

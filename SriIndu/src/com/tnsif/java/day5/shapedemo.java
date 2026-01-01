c2664a550e4e5967e2d6999e726122f024cae7c2
manideep <vaishnavi@MANIDEEP>
2026-01-01 23:02:42 +0530

    shape.java shapedemo.java

---
+++ SriIndu/src/com/tnsif/java/day5/shapedemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.java.day5;
+
+public class shapedemo {
+
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle();
+		r.setl(4);
+		r.setb(9);
+		r.calarea();
+		
+		System.out.println("LENGTH:"+r.getb());
+		System.out.println("BREADTH:"+r.getl());
+		
+		square s = new square();
+		s.sets(8);
+		s.calarea();
+		
+		System.out.println("SQUARE SIDES:"+s.gets());
+		
+			
+		}
+
+	}
+
+

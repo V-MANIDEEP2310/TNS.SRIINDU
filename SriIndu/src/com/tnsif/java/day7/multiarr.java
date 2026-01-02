db93503ec3555584740ebceb8e1f0df982c75093
manideep <vaishnavi@MANIDEEP>
2026-01-02 23:01:02 +0530

    singlearr.java multiarr.java jaggedarr.java

---
+++ SriIndu/src/com/tnsif/java/day7/multiarr.java
@@ -0,0 +1,33 @@
+package com.tnsif.java.day7;
+
+public class multiarr {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[3][3];
+		
+		arr[0][0]=1;
+		arr[0][1]=6;
+		arr[0][2]=4;
+		
+		arr[1][0]=8;
+		arr[1][1]=2;
+		arr[1][2]=9;
+		
+		arr[2][0]=5;
+		arr[2][1]=7;
+		arr[2][2]=3;
+		
+		for(int i = 0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				System.out.print(arr[i] [j]+"    ");
+			}	
+			System.out.println();
+			
+		}
+		
+	}
+}	
+	
+
+
+ 
---
+++ SriIndu/src/com/tnsif/java/day7/singlearr.java
@@ -0,0 +1,22 @@
+package com.tnsif.java.day7;
+
+public class singlearr {
+
+	public static void main(String[] args) {
+		int[] arr= new int [5];
+		arr[0]=9;
+		arr[1]=8;
+		arr[2]=7;
+		arr[3]=6;
+		arr[4]=5;
+		
+		for(int i=0;i<=4;i++) {
+			System.out.println(arr[i]);
+		}
+	
+		
+	
+
+	}
+
+}

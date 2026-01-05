fdd9ea113ef25f1b5bbeeb119058df693b945d0f
manideepi <vaishnavi@MANIDEEP>
2026-01-05 23:25:06 +0530

    typeconversion.java

---
+++ SriIndu/src/com/tnsif/assignments1/typeconversion.java
@@ -0,0 +1,13 @@
+package com.tnsif.assignments1;
+
+public class typeconversion {
+	public static void main(String [] args) {
+	int a = 555;
+    double b = a;
+    System.out.println(b);
+}
+	
+		
+	}
+
+

fbf9b5f112e366b82aa7c5639a94251410be49cc
manideep <vaishnavi@MANIDEEP>
2026-01-19 23:07:44 +0530

    arraylist.java linkedlist.java

---
+++ SriIndu/src/com/tnsif/java/day10/arraylist.java
@@ -0,0 +1,18 @@
+package com.tnsif.java.day10;
+
+import java.util.ArrayList;
+
+public class arraylist {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(59);
+		al.add(101);
+		al.add(45667);
+		System.out.println(al);
+		
+		
+
+	}
+
+}
---
+++ SriIndu/src/com/tnsif/java/day10/linkedlist.java
@@ -0,0 +1,18 @@
+package com.tnsif.java.day10;
+
+import java.util.LinkedList;
+
+public class linkedlist {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> al = new LinkedList<Integer>();
+		al.addFirst(1);
+		
+		al.addLast(123);
+		
+		System.out.println(al);
+		
+
+	}
+
+}
